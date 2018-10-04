ab76ba6e6e4835a501ef34149f848b7c28204010
Aaliyah Zaguirre <ajzaguirre@yahoo.com>
2018-10-04 14:50:13 +0100

    aaliyahs work

--- app/App.js
+++ app/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry, Button} from 'react-native';
 
-export default class App extends React.Component {
+export default class AlignItemsBasics extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+      }}>
+        <View style={{width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: 'center', backgroundColor: 'powderblue'}}>
+          <Text style={{
+            fontFamily : "Arial",
+            fontSize : "50%",
+            fontWeight : "bold",
+            textAlign: "center",
+          }}>Find 
+          {"\n"}Your 
+          {"\n"}Mind </Text>
+          <Text></Text>
+          <Button
+            onPress={''}
+            title="Anonymous Chat"
+            color="#194D33"
+            style={{
+              left: "0",
+              position: 'absolute'
+            }}
+            accessibilityLabel="Anonymous Chat"
+          />
+        </View> 
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
